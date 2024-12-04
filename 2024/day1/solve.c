2e22b4c02d296897239224a65a4c44199d4859e7
jlt525 <35385142+jlt525@users.noreply.github.com>
2024-12-04 22:22:01 +0000

    solved 2024d01

--- 2024/day1/solve.c
+++ 2024/day1/solve.c
@@ -1,16 +1,48 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INPUT_FILE "input"
+
+void generate_lists(char* input_file, int (*list1)[1000], int (*list2)[1000]);
+void bubble_sort_list(int (*list)[1000]);
+
 int main()
 {
-	char input_line[15], string_num1[6], string_num2[6];
-	long int  num1, num2, list1[1000], list2[1000];
+	int i, c, counter, similarity_score, list1[1000], list2[1000], total_distance;
+	
+	generate_lists(INPUT_FILE, &list1, &list2);
+	bubble_sort_list(&list1);
+	bubble_sort_list(&list2);
+
+	for(i = 0; i < 1000; i++)
+	{
+		counter = 0;
+		for(c = 0; c < 1000; c++)
+		{
+			if(list1[i] == list2[c])
+			{
+				counter++;
+			}
+
+		}
+		
+		similarity_score += list1[i] * counter;
+		total_distance += abs(list1[i] - list2[i]);
+	}
+	
+	printf("%d\t\t%d\n", total_distance, similarity_score);
+
+	return 0;
+}
+
+void generate_lists(char input_file[6], int (*list1)[1000], int (*list2)[1000])
+{
 	int list_position = 0;
+	char input_line[15], string_num1[6], string_num2[6];
 	char *endptr;
-
 	FILE *file_pointer;
-	file_pointer = fopen("input", "r");
-
+	file_pointer = fopen(input_file, "r");
 
 	while(fgets(input_line, 15, file_pointer))
 	{
@@ -18,22 +50,43 @@ int main()
 		for(i = 0; i < 5; i++)
 		{
 			string_num1[i] = input_line[i];
-			string_num1[5] = '\0';
+			string_num2[i] = input_line[i + 8];
+			string_num2[5] = string_num1[5] = '\0';
 		}
 
-		for(i = 12; i > 7; i--)
-		{
-			string_num2[i - 8] = input_line[i];
-			string_num2[5] = '\0';
-		}
-
-		list1[list_position] = strtol(string_num1, &endptr, 10);
-		list2[list_position] = strtol(string_num2, &endptr, 10);
+		(*list1)[list_position] = (int)strtol(string_num1, &endptr, 10);
+		(*list2)[list_position] = (int)strtol(string_num2, &endptr, 10);
 
 		list_position++;
 	}
 
-	fclose(file_pointer);
+	(void)fclose(file_pointer);
+}
 
-	return 0;
+void bubble_sort_list(int (*list)[1000])
+{
+	int list_length = 1000, list_position, tmp_int = 0;
+	bool sorted = false;
+
+	while(sorted != true)
+	{
+		for(list_position = 0; list_position < list_length - 1; list_position++)
+		{
+			if((*list)[list_position] > (*list)[list_position + 1])
+			{
+				tmp_int = (*list)[list_position];
+				(*list)[list_position] = (*list)[list_position + 1];
+				(*list)[list_position + 1] = tmp_int;
+			}
+		}
+		sorted = true;
+
+		for(list_position = 0; list_position < list_length - 1; list_position++)
+		{
+			if((*list)[list_position] > (*list)[list_position + 1])
+			{
+				sorted = false;
+			}
+		}
+	}
 }
