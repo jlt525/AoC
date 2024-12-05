bad7f74aa845f14a98f326e0383f646647d0ce06
jlt525 <35385142+jlt525@users.noreply.github.com>
2024-12-05 21:19:52 +0000

    day02 done

--- 2024/day02/solve.c
+++ 2024/day02/solve.c
@@ -5,16 +5,17 @@
 
 int count_safe_lines(char *input_file);
 bool line_is_safe(int (*line)[], int line_length);
+bool problem_dampener(int (*line)[], int line_length);
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", count_safe_lines(argv[1]));
+	printf("safe_lines: %d\n", count_safe_lines(argv[1]));
 	return 0;
 }
 
 int count_safe_lines(char *input_file)
 {
-	int line = 1, safe_lines, c, i, int_line_position, int_line[100];
+	int line = 1, safe_lines = 0, c, i, int_line_position, int_line[100];
 	char input_line[100], string_num[5];
 	char *endptr;
 	FILE *file_pointer;
@@ -43,12 +44,19 @@ int count_safe_lines(char *input_file)
 			i++;
 		}
 
+		printf("line: %d\n", line);
 		if(line_is_safe(&int_line, int_line_position))
 		{
 			safe_lines++;
 			printf("line %5d safe\n", line);
+			printf("total safe lines: %d\n\n", safe_lines);
+		} else if(problem_dampener(&int_line, int_line_position))
+		{
+			safe_lines++;
+			printf("line %5d dampened\n", line);
+			printf("total safe line: %d\n\n", safe_lines);
 		} else {
-			printf("line %5d unsafe\n", line);
+			printf("line %5d unsafe\n\n", line);
 		}
 		line++;
 	}
@@ -61,28 +69,44 @@ int count_safe_lines(char *input_file)
 bool line_is_safe(int (*line)[], int line_length)
 {
 	int c, i, prev_direction, direction, diff, differences[100];
-	bool one_direction = true, close_diffs = true;
+	bool one_direction = true, close_diffs = true, no_0_diff = true;
+
+	for(i = 0; i < line_length; i++)
+	{
+		printf("\t%d", (*line)[i]);
+	}
 
 	for(i = 0; i < (line_length - 1); i++)
 	{
 		differences[i] = (*line)[i + 1] - (*line)[i];
 	}
 
+	printf("\n");
+	
+	for(c = 0; c < i; c++)
+	{
+		printf("\t%5d", differences[c]);
+	}
+
+	printf("\n");
+
 	if(differences[0] < 0)
 	{
+		printf("\tdir: decr\n");
 		prev_direction = 0;
 	} else if(differences[0] > 0) {
+		printf("\tdir: incr\n");
 		prev_direction = 1;
 	} else {
+		printf("\tdir: none\n");
 		return false;
 	}
 
 	for(c = 0; c < i; c++)
 	{
-		printf("%d", c);
 		if((abs(differences[c]) < 1) || (3 < abs(differences[c])))
 		{
-			printf("close_diffs\n");
+			printf("\tclose_diffs\n");
 			return false;
 		}
 
@@ -95,10 +119,38 @@ bool line_is_safe(int (*line)[], int line_length)
 
 		if(direction != prev_direction)
 		{
-			printf("one_direction\n");
+			printf("\tone_direction\n");
 			return false;
 		}
 	}
 
 	return true;
+}
+
+bool problem_dampener(int (*line)[], int line_length)
+{
+	int i, element_to_skip, new_line[100], new_line_length;
+
+	for(element_to_skip = 0; element_to_skip < line_length; element_to_skip++)
+	{
+		new_line_length = 0;
+
+		for(i = 0; i < line_length ; i++)
+		{
+			if(i != element_to_skip)
+			{
+				new_line[new_line_length] = (*line)[i];
+				printf("%d ", new_line[new_line_length]);
+				new_line_length++;
+			}
+		}
+		printf("\n");
+
+		if(line_is_safe(&new_line, new_line_length))
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
